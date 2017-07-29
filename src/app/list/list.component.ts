69cd002b778f9145ab4f6c5d727ce79a6d9347a1
unknown <fr0gm4ch1n3@gmail.com>
2017-07-29 14:37:29 +0200

    Add list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data/data.service';
 
 @Component({
   selector: 'app-list',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 
-export class ListComponent { }
+export class ListComponent  implements OnInit {
+  public errorMessage: string;
+  public users: any[] = [];
+
+  /**
+   * Creates an instance of the ListComponent with the injected
+   * DataService.
+   *
+   * @param {DataService} dataService - The injected DataService.
+   */
+  constructor(private dataService: DataService) {}
+
+  /**
+   * Get the users OnInit
+   */
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  /**
+   * Handle the dataService observable
+   */
+  getUsers() {
+    this.dataService.getUsers()
+      .subscribe(
+        users => this.users = users,
+        error => this.errorMessage = <any>error
+      );
+  }
+}
--- src/app/shared/data/data.service.ts
+++ src/app/shared/data/data.service.ts
@@ -4,4 +4,58 @@ import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/operator/do';  // for debugging
 
 @Injectable()
-export class DataService {}
+export class DataService {
+  private endoints = {
+    users: 'http://jsonplaceholder.typicode.com/users'
+  };
+
+  /**
+   * Creates a new DataService with the injected Http.
+   * @param {Http} http - The injected Http.
+   * @constructor
+   */
+  constructor(private http: Http) {}
+
+  /**
+   * Returns an Observable for the HTTP GET request for the JSON resource.
+   * @return {any[]} The Observable for the HTTP request.
+   */
+  getUsers(): Observable<{
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: {
+      street: string,
+      suite: string,
+      city: string,
+      zipcode: string,
+      geo: {
+        lat: string,
+        lng: string
+      }
+    },
+    phone: string,
+    website: string,
+    company: {
+      name: string,
+      catchPhrase: string,
+      bs: string
+    }
+  }[]> {
+    return this.http.get(this.endoints.users)
+                    .map((res: Response) => res.json())
+    //              .do(data => console.log('server data:', data))  // debug
+                    .catch(this.handleError);
+  }
+
+  /**
+   * Handle HTTP error
+   */
+  private handleError (error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
